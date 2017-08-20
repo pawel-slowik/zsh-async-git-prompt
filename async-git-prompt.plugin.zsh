e91540c6bb9a5d648074b523c368066d8c1afeda
Paweł Słowik <pawel.slowik@zoho.com>
2017-08-21 00:21:30 +0200

    Fix scope of the `line` variable - make it local
    Caught by running with WARN_CREATE_GLOBAL enabled.

--- async-git-prompt.plugin.zsh
+++ async-git-prompt.plugin.zsh
@@ -166,7 +166,7 @@ function git_get_tag_or_hash() {
 }
 
 function git_prompt_completed_callback() {
-	local symbol buffer=""
+	local symbol line buffer=""
 	while read -t 0 -r -u 3 line; do
 		eval $line
 	done
